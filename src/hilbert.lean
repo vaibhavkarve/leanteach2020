10752025df6066835e85cc1f0aaec7f9d3113fcd
Vaibhav Karve <vaibhavskarve@gmail.com>
2020-06-18 09:15:00 -0500

    hilbert : add axiom C_segment_symm. tag C_segment_refl with @[refl]

--- src/hilbert.lean
+++ src/hilbert.lean
@@ -189,13 +189,14 @@ axiom segment_copy' (a b a' : Point) (l l' : Line)
 
 
 -- III.1 Part 2
+@[symm] axiom C_segment_symm (s₁ s₂ : Segment) : s₁ ≃ s₂ → s₂ ≃ s₁
 @[symm] axiom C_segment_swap (x y : Point) : x⬝y ≃ y⬝x
 
 -- III.2
 @[trans] axiom C_segment_trans (u v w x y z : Point) : (u⬝v ≃ z⬝w) → (u⬝v ≃ x⬝y) → z⬝w ≃ x⬝y
 
 -- Congruence of segments is reflexive
-lemma C_segment_refl (a b : Point) : a⬝b ≃ a⬝b :=
+@[refl] lemma C_segment_refl (a b : Point) : a⬝b ≃ a⬝b :=
 begin
   fapply C_segment_trans,
     use b,
